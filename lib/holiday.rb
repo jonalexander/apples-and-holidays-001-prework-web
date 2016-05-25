3b6d03710f3c71c37778527a76d5a2ef3cbcec9b
Jonathan Alexander <jonathandalexander@gmail.com>
2016-05-25 16:16:49 -0400

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,6 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].values.flatten
 end
 
-
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize}:"
@@ -34,8 +33,6 @@ def all_supplies_in_holidays(holiday_hash)
     end
   end
 end
-
-
 
 def all_holidays_with_bbq(holiday_hash)
   holiday_array = []
